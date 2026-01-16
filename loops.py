c8e4403fe8b4d5bd4436a1471ce0e3f11cc738e7
Bhoomika Betegowda <159523352+bhoomikagowda-b@users.noreply.github.com>
2026-01-16 19:11:16 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,11 @@
+# simple_loops.py
+
+# For loop
+for i in range(5):
+    print("For loop iteration:", i)
+
+# While loop
+count = 0
+while count < 5:
+    print("While loop count:", count)
+    count += 1
